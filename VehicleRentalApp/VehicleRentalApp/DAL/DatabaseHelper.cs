661959b80f3ead988af7ab4f44cdb48a476efdca
kim2817 <karma.adnan.hussein@gmail.com>
2026-04-28 11:03:56 +0300

    program STructure

---
+++ VehicleRentalApp/VehicleRentalApp/DAL/DatabaseHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace VehicleRentalApp.DAL
+{
+    public class DatabaseHelper
+    {
+        private static string connString =
+            ConfigurationManager.ConnectionStrings["VehicleRentalDB"].ConnectionString;
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connString);
+        }
+    }
+}
