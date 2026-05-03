b314e79c6ec38710fc779efda55e6353efcf1601
kim2817 <karma.adnan.hussein@gmail.com>
2026-05-03 03:02:24 +0300

    carcard hopefully completo

--- VehicleRentalApp/VehicleRentalApp/Cars.cs
+++ VehicleRentalApp/VehicleRentalApp/Cars.cs
@@ -64,34 +64,34 @@ namespace VehicleRentalApp
 
         private void LoadCars()
         {
-            //int branchID = this.branch.Branch_ID;
+            int branchID = this.branch.Branch_ID;
 
-            //// Call backend with current filter state
-            //Car[] carsFromDB = VHSAUTOMOTIVE.filter_Cars_In_Branch(branchID,statusFilter,picantoChecked,suvChecked,coupeChecked,sedanChecked,hatchbackChecked
-            //);
+            // Call backend with current filter state
+            Car[] carsFromDB = VHSAUTOMOTIVE.filter_Cars_In_Branch(branchID, statusFilter, picantoChecked, suvChecked, coupeChecked, sedanChecked, hatchbackChecked);
 
-            //flowLayoutPanel1.Controls.Clear();
+            flowLayoutPanel1.Controls.Clear();
 
-            //foreach (var carData in carsFromDB)
-            //{
-            //    var car = new CarInfo
-            //    {
-            //        LicensePlate = carData.License_Plate,
-            //        Condition = carData.Condition,
-            //        Seats = carData.No_seats,
-            //        Mileage = carData.Mileage,
-            //        Colour = carData.Colour,
-            //        CarType = carData.Car_Type,
-            //        Make = carData.Make,
-            //        Model = carData.Model,
-            //        ModelYear = carData.Model_Year,
-            //        Transmission = carData.Transmission,
-            //        DailyRate = carData.Daily_Rental_Rate,
-            //        IsReserved = carData.Condition == "Reserved"
-            //    };
+            foreach (var carData in carsFromDB)
+            {
+                Car_Category carCat = VHSAUTOMOTIVE.cat_fromID(carData.Category_ID);
+                var car = new CarInfo
+                {
+                    LicensePlate = carData.License_Plate,
+                    Condition = carData.Condition,
+                    Seats = carData.No_seats,
+                    Mileage = carData.Mileage,
+                    Colour = carData.Colour,
+                    CarType = carCat.Car_Type,
+                    Make = carCat.Make,
+                    Model = carCat.Model,
+                    ModelYear = carCat.Model_Year,
+                    Transmission = carCat.Transmission,
+                    DailyRate = carCat.Daily_Rental_Rate,
+                    IsReserved = carData.Condition == "Reserved"
+                };
 
-            //    flowLayoutPanel1.Controls.Add(new CarCard(car));
-            //}
+                flowLayoutPanel1.Controls.Add(new CarCard(car));
+            }
         }
 
         private void right_Click(object sender, EventArgs e)
